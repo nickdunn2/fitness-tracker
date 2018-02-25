deafcb4612f8cd8abe4af20af3b8d4a7dafc5676
nickdunn2 <nick.dunn@flosports.tv>
2018-02-24 19:19:56 -0600

    feat: introduce login form

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,15 +1,29 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core'
+import { FormBuilder, Validators } from '@angular/forms'
+import { validateEmail } from '../../shared/validators/email.validator'
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
-export class LoginComponent implements OnInit {
+export class LoginComponent {
+  public loginForm = this.fb.group({
+      'email': ['', [
+          Validators.required,
+          Validators.minLength(5),
+          validateEmail
+        ]
+      ],
+      'password': ['', [ Validators.required ]]
+    })
 
-  constructor() { }
+  public get email() { return this.loginForm.get('email') }
+  public get password() { return this.loginForm.get('password') }
 
-  ngOnInit() {
+  constructor(private fb: FormBuilder) { }
+
+  submitForm() {
+    console.log('loginForm --', this.loginForm)
   }
-
 }
