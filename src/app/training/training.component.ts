20ee17ab504de01f939680699d9703159738a096
nickdunn2 <nick.dunn@flosports.tv>
2018-03-04 20:20:19 -0600

    fix: change valueChanges() to snapshotChanges()

--- src/app/training/training.component.ts
+++ src/app/training/training.component.ts
@@ -3,6 +3,7 @@ import { TrainingService } from './training.service'
 import { Subscription } from 'rxjs/Subscription'
 import { AngularFirestore } from 'angularfire2/firestore'
 import { Observable } from 'rxjs/Observable'
+import { Exercise } from './exercise.model'
 
 @Component({
   selector: 'app-training',
@@ -11,14 +12,25 @@ import { Observable } from 'rxjs/Observable'
 })
 export class TrainingComponent implements OnInit, OnDestroy {
   public ongoingTraining = false
-  public exercises: Observable<any>
+  public exercises: Observable<Exercise[]>
   public exerciseSub: Subscription
 
   constructor(private trainingService: TrainingService, private db: AngularFirestore) { }
 
   ngOnInit() {
-    // this.exercises = this.trainingService.availableExercises
-    this.exercises = this.db.collection('availableExercises').valueChanges()
+    this.exercises = this.db
+      .collection('availableExercises')
+      .snapshotChanges()
+      .map(resultsArray => {
+        return resultsArray.map(res => {
+          return {
+            id: res.payload.doc.id,
+            name: res.payload.doc.data().name,
+            duration: res.payload.doc.data().duration,
+            caloriesBurned: res.payload.doc.data().caloriesBurned
+          }
+        })
+      })
 
     this.exerciseSub = this.trainingService.exerciseChanged.subscribe(ex => {
       this.ongoingTraining = !!ex
