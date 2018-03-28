ed7d9fe8acefb3e468b2391e574d54075390f661
nickdunn2 <nick.dunn@flosports.tv>
2018-03-28 09:39:03 -0500

    refactor: separate out AuthModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser'
 import { NgModule } from '@angular/core'
-import { ReactiveFormsModule } from '@angular/forms'
 import { FlexLayoutModule } from '@angular/flex-layout'
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
 import { AngularFireModule } from 'angularfire2'
@@ -9,8 +8,6 @@ import { AngularFireAuthModule } from 'angularfire2/auth'
 
 import { AppComponent } from './app.component'
 import { MaterialModule } from './material.module'
-import { SignupComponent } from './auth/signup/signup.component'
-import { LoginComponent } from './auth/login/login.component'
 import { TrainingComponent } from './training/training.component'
 import { CurrentTrainingComponent } from './training/current-training/current-training.component'
 import { NewTrainingComponent } from './training/new-training/new-training.component'
@@ -24,13 +21,12 @@ import { AuthService } from './auth/auth.service'
 import { TrainingService } from './training/training.service'
 import { environment } from '../environments/environment'
 import { UIService } from './shared/ui.service'
+import { AuthModule } from './auth/auth.module'
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    SignupComponent,
-    LoginComponent,
     TrainingComponent,
     CurrentTrainingComponent,
     NewTrainingComponent,
@@ -46,10 +42,10 @@ import { UIService } from './shared/ui.service'
     MaterialModule,
     AppRoutingModule,
     FlexLayoutModule,
-    ReactiveFormsModule,
     AngularFireModule.initializeApp(environment.firebase),
     AngularFirestoreModule,
-    AngularFireAuthModule
+    AngularFireAuthModule,
+    AuthModule
   ],
   providers: [
     AuthService,
